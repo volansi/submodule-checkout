bba3b314075295b076e484098e8b1b4d286140bf
JacobCrabill <jacob@flyvoly.com>
2021-09-10 19:17:52 -0700

    forgot to add token arg to recursive call

--- recurse-submodules.sh
+++ recurse-submodules.sh
@@ -5,6 +5,6 @@ if [[ `grep "url" .gitmodules 2>/dev/null` ]]; then
 	sed -E "s/git@github.com:|https:\/\/github.com\//https:\/\/$token:$token@github.com\//" -i .gitmodules
 	git submodule sync
 	git submodule update --init
-	git submodule foreach --quiet 'cp $toplevel/recurse-submodules.sh . && bash $toplevel/recurse-submodules.sh || :'
+	git submodule foreach --quiet "cp $toplevel/recurse-submodules.sh . && bash $toplevel/recurse-submodules.sh $token || :"
 fi
 
