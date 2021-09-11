3b13cac58b98166062b854fef7de9d292d741c70
JacobCrabill <jacob@flyvoly.com>
2021-09-10 19:28:21 -0700

    Try fixing path to script once recursion starts

--- recurse-submodules.sh
+++ recurse-submodules.sh
@@ -1,10 +1,12 @@
 #/bin/env bash
 token=$1
+adir=$2
 echo `pwd`
+echo $adir
 if [[ `grep "url" .gitmodules 2>/dev/null` ]]; then
 	sed -E "s/git@github.com:|https:\/\/github.com\//https:\/\/$token:$token@github.com\//" -i .gitmodules
 	git submodule sync
 	git submodule update --init
-	git submodule foreach --quiet "cp $toplevel/recurse-submodules.sh . && bash $toplevel/recurse-submodules.sh $token || :"
+	git submodule foreach --quiet "bash $adir/recurse-submodules.sh $token $adir || :"
 fi
 
